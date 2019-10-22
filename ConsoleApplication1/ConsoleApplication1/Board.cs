03b09bb03d61533f527005cdc5086563dcaecd3d
minhoo1505 <teamtheweeknd2016@gmail.com>
2019-10-22 15:12:06 +0700

    lk

---
+++ ConsoleApplication1/ConsoleApplication1/Board.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Board
+    {
+        private int G1, G2, G3;
+        public Board(int g1,int g2,int g3)
+        {
+            this.G1 = g1;
+            this.G2 = g2;
+            this.G3 = g3;
+        }
+        public void PickBall(int g,int b)
+        {
+            switch(g)
+            {
+                case 1:
+                    G1 -= b;
+                    break;
+                case 2:
+                    G2 -= b;
+                    break;
+                case 3:
+                    G3 -= b;
+                    break;
+            }
+
+        }
+        public void printGame()
+        {
+            Console.Write("Group 1: ");
+            for(int i=0;i<G1;i++)
+            {
+                Console.Write(" o ");
+            }
+            Console.WriteLine();
+            Console.Write("Group 2: ");
+            for (int i = 0; i < G2; i++)
+            {
+                Console.Write(" o ");
+            }
+            Console.WriteLine();
+            Console.Write("Group 3: ");
+            for (int i = 0; i < G3; i++)
+            {
+                Console.Write(" o ");
+            }
+            Console.WriteLine();
+        }
+        public bool isgameover()
+        {
+            if (G1 == 0 && G2 == 0 && G3 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class controller
+    {
+        private Board Game;
+        private Player P1, P2;
+        public controller()
+        {
+            P1 = new Player("kha");
+            P2 = new Player("nhan");
+            Game = new Board(3, 4, 6);
+        }
+        public void playgame()
+        {
+            Game.printGame();
+            while(true)
+            {
+                Console.WriteLine(P1.GetName() + "a turn ");
+                P1.PickBall(Game);
+                Game.printGame();
+                if(Game.isgameover())
+                {
+                    Console.WriteLine(P1.GetName() + "loses ");
+                    Console.WriteLine(P2.GetName() + " wins ");
+                    break;
+                }
+                Console.WriteLine(P2.GetName() + "a turn");
+                P2.PickBall(Game);
+                Game.printGame();
+                if(Game.isgameover())
+                {
+                    Console.WriteLine(P2.GetName() + "loses ");
+                    Console.WriteLine(P1.GetName() + " wins ");
+                    break;
+                }
+            }
+        }
+    }
+}
